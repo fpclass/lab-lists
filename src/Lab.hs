15e65a06bab0fd855b50a31b400c370e0dcfe07e
Michael Gale <m.gale@warwick.ac.uk>
2021-01-28 19:28:18 +0000

    Add solutions

--- src/Lab.hs
+++ src/Lab.hs
@@ -18,19 +18,25 @@ numbers = 4 : 8 : 15 : 16 : 23 : 42 : []
 -- with explicit brackets to show the associativity of cons
 -- the number 4 is currently missing from the beginning of this list
 numbers' :: [Int]
-numbers' = 8 : (15 : (16 : 23 : (42 : [])))
+numbers' = 4 : (8 : (15 : (16 : (23 : (42 : [])))))
 
 -- syntactic sugar for lists
 -- the number 42 is currently missing from the end of this list
 numbers'' :: [Int]
-numbers'' = [4,8,15,16,23]
+numbers'' = [4,8,15,16,23,42]
 
 --------------------------------------------------------------------------------
 
 isPalindrome :: [Char] -> Bool 
-isPalindrome = undefined
+isPalindrome xs = xs == reverse xs
 
-validModuleCode :: [Char] -> Bool 
-validModuleCode = undefined
+validModuleCode :: [Char] -> Bool
+validModuleCode ['c','s',x,y,z] =
+    isDigit x && isDigit y && isDigit z
+-- validModuleCode ['c','s',x,y,z] =
+--     all isDigit [x,y,z]
+-- validModuleCode ('c':'s':xs) = 
+--     length xs == 3 && all isDigit xs
+validModuleCode _ = False
 
 --------------------------------------------------------------------------------
